af1b6d46379f082c5452859f51e26632ac42541a
VimeshLunagariya <vimeshlunagariya7@gmail.com>
2023-10-05 19:07:42 +0530

    up to merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_links/app_links.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
